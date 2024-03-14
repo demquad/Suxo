0716b69b87ee7fd6aec22de78cdefc3e456f7953
ISAAC366 <109168136+ISAAC366@users.noreply.github.com>
2024-03-14 11:07:05 -0600

    re-upload

---
+++ Sux/Tables/themes.lua
@@ -0,0 +1,63 @@
+local ThemesList = {
+    Default = {
+        Name = "Default";
+        MainColor = Color3.fromRGB(43, 124, 255);
+        Background = Color3.fromRGB(29,29,29);
+        Elements = Color3.fromRGB(26,26,26);
+        Tabs = Color3.fromRGB(32,32,32);
+        TextColor = Color3.fromRGB(255,255,255);
+        LeftWindowCo = Color3.fromRGB(25,25,25);
+        IconsColor = Color3.fromRGB(38,38,38);
+        WindowTabColor = Color3.fromRGB(21,21,21);
+        DropHolder = Color3.fromRGB(15,15,15);
+        ToggleDropColor = Color3.fromRGB(21,21,21);
+        IconsImageColor = Color3.fromRGB(255,255,255);
+        BackgroundElements = Color3.fromRGB(34,34,34);
+    };
+    ElectricVioletTheme = {
+        Name = "ElectricVioletTheme";
+        MainColor = Color3.fromRGB(138, 43, 226);
+        Background = Color3.fromRGB(28, 28, 28); 
+        Elements = Color3.fromRGB(24, 24, 24); 
+        Tabs = Color3.fromRGB(30, 30, 30); 
+        TextColor = Color3.fromRGB(220, 220, 220); 
+        LeftWindowCo = Color3.fromRGB(23, 23, 23); 
+        IconsColor = Color3.fromRGB(35, 35, 35); 
+        WindowTabColor = Color3.fromRGB(19, 19, 19); 
+        DropHolder = Color3.fromRGB(14, 14, 14); 
+        ToggleDropColor = Color3.fromRGB(20, 20, 20); 
+        IconsImageColor = Color3.fromRGB(240, 240, 240);
+        BackgroundElements = Color3.fromRGB(32, 32, 32); 
+    };
+    ElectricRedTheme = {
+        Name = "ElectricRedTheme";
+        MainColor = Color3.fromRGB(255, 69, 16); 
+        Background = Color3.fromRGB(28, 28, 28);
+        Elements = Color3.fromRGB(24, 24, 24);
+        Tabs = Color3.fromRGB(30, 30, 30); 
+        TextColor = Color3.fromRGB(220, 220, 220); 
+        LeftWindowCo = Color3.fromRGB(23, 23, 23); 
+        IconsColor = Color3.fromRGB(35, 35, 35); 
+        WindowTabColor = Color3.fromRGB(19, 19, 19); 
+        DropHolder = Color3.fromRGB(14, 14, 14); 
+        ToggleDropColor = Color3.fromRGB(20, 20, 20); 
+        IconsImageColor = Color3.fromRGB(240, 240, 240);
+        BackgroundElements = Color3.fromRGB(32, 32, 32); 
+    };
+    Midnight = {
+        Name = "Midnight";
+        MainColor = Color3.fromRGB(28, 196, 151); 
+        Background = Color3.fromRGB(44, 62, 82);
+        Elements = Color3.fromRGB(14, 102, 79);
+        Tabs = Color3.fromRGB(16, 100, 80);
+        TextColor = Color3.fromRGB(220, 220, 220); 
+        LeftWindowCo = Color3.fromRGB(53, 74, 97); 
+        IconsColor = Color3.fromRGB(18, 94, 72); 
+        WindowTabColor = Color3.fromRGB(53, 74, 97); 
+        DropHolder = Color3.fromRGB(10, 59, 46); 
+        ToggleDropColor = Color3.fromRGB(10, 59, 46); 
+        IconsImageColor = Color3.fromRGB(240, 240, 240);
+        BackgroundElements = Color3.fromRGB(9, 61, 47); 
+    };
+}
+return ThemesList
